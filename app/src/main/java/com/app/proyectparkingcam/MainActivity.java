4224c902c5517aaa0be4bb9eb2d5de49161d0d2a
Ariel Aguirre <ariel.aguirre.est@tecazuay.edu.ec>
2023-03-03 07:03:50 -0500

    validacion_usuario

--- app/src/main/java/com/app/proyectparkingcam/MainActivity.java
+++ app/src/main/java/com/app/proyectparkingcam/MainActivity.java
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -43,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
         editxtClave = (EditText) findViewById(R.id.edittxtClave);
         editxtUser = (EditText)findViewById(R.id.edittxtUser);
         txtMensaje = (TextView) findViewById(R.id.txtViewMensaje);
+        //VALIDAR LIMITE CARACTERRES
+        //Se añade el TextWatcher al EditText
+        editxtUser.addTextChangedListener(mTextEditorWatcher);
+        editxtClave.addTextChangedListener(mTextEditorWatcher);
         //PETICION GET
         //TextUtils.isEmpty(editText.getText().toString())
          btnInicio = findViewById(R.id.btnLogin);
@@ -52,13 +59,22 @@ public class MainActivity extends AppCompatActivity {
                 if(TextUtils.isEmpty(editxtUser.getText().toString()) ||TextUtils.isEmpty(editxtClave.getText().toString())){
                     Campos_vacios_incorrectos(1);
 
-                }else{
+                }if(editxtUser.length() < 10){
+                    Campos_vacios_incorrectos(4);
+
+                }if(editxtClave.length()<6){
+                    Campos_vacios_incorrectos(5);
+
+                }
+
+                else{
                     Listar();
                 }
 
 
             }
         });
+
 
    }
     
@@ -114,7 +130,15 @@ public class MainActivity extends AppCompatActivity {
         }if(valor==3){
             mensaje="Error del servidor o dominio";
             Borrar_campos(1);
+        }if(valor==4){
+            mensaje="No se admiten menos de 10 caracteres en en el usuario";
+            Borrar_campos(1);
         }
+        if(valor==5){
+            mensaje="Escriba una clave minima de 6 caracteres";
+
+        }
+
         AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
         alerta.setMessage(mensaje).setCancelable(false).setPositiveButton(
                 "OK", new DialogInterface.OnClickListener() {
@@ -148,5 +172,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    // Se crea un TextWatcher
+    TextWatcher mTextEditorWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //Establece el límite de caracteres del EditText
+            int limiteCaracteres=10;
+            // Si el usuario ha llegado al límite de caracteres, muestra un mensaje
+            if (s.length() > limiteCaracteres) {
+                Toast.makeText(getApplicationContext(), "Has llegado al límite de caracteres", Toast.LENGTH_SHORT).show();
+            } // Si el usuario ha llegado al límite de caracteres, borra los últimos caracteres
+            if (s.length() > limiteCaracteres) {
+                editxtUser.setText(s.subSequence(0, limiteCaracteres));
+                editxtUser.setSelection(limiteCaracteres);
+
+            }
+            if (editxtClave.length() > limiteCaracteres) {
+                editxtClave.setText(s.subSequence(0, limiteCaracteres));
+                editxtClave.setSelection(limiteCaracteres);
+            }
+
+        }
+
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+
 
 }
