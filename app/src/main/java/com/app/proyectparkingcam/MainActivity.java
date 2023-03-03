dfb84df576303938ad86f2b83ea009748c93f589
Ariel Aguirre <ariel.aguirre.est@tecazuay.edu.ec>
2023-03-02 23:59:21 -0500

    Cambios_User_Validation

--- app/src/main/java/com/app/proyectparkingcam/MainActivity.java
+++ app/src/main/java/com/app/proyectparkingcam/MainActivity.java
@@ -20,56 +20,13 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    TextView txtPlazasB1,txtPlazasB2,txtNumPlazas,txtRegistro;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        txtPlazasB1 = findViewById(R.id.txtIdPlazas);
-        txtNumPlazas = findViewById(R.id.txtNumPlazas);
-        txtRegistro = findViewById(R.id.txtRegistro);*/
         Button btnInicio = findViewById(R.id.btnLogin);
 
 
-        btnInicio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent it = new Intent(MainActivity.this, Menu.class);
-                startActivity(it);
-                //LeerWs();
-            }
-        });
-
     }
-/*
-    private void LeerWs() {
 
-        String url = "https://19b5-181-211-10-245.sa.ngrok.io/api/block/search/1";
-
-        StringRequest postResquest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    txtPlazasB1.setText(jsonObject.getString("id_bloque"));
-                    String plazas = jsonObject.getString("plazas");
-                    txtRegistro.setText(plazas);
-                    String registro = jsonObject.getString("registro");
-                    txtRegistro.setText(registro);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Error", error.getMessage());
-            }
-        });
-        Volley.newRequestQueue(this).add(postResquest);
-    }*/
 }
